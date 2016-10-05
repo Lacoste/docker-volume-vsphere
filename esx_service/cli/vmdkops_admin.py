4b79f86ba7d222579602474fede02ee5f76f85a3
govint <administrator@hte-1s-eng-dhcp98.eng.vmware.com>
2016-10-05 03:56:15 -0700

    Fix for error handling on set command in admin cli.

--- esx_service/cli/vmdkops_admin.py
+++ esx_service/cli/vmdkops_admin.py
@@ -563,10 +563,13 @@ def status(args):
 
 
 def set_vol_opts(args):
-    set_ok = vmdk_ops.set_vol_opts(args.volume, args.options) 
-    if set_ok:
-        print('Successfully updated settings for : {0}'.format(args.volume))
-    else:
+    try:
+        set_ok = vmdk_ops.set_vol_opts(args.volume, args.options) 
+        if set_ok:
+           print('Successfully updated settings for : {0}'.format(args.volume))
+        else:
+           print('Failed to update {0} for {1}.'.format(args.options, args.volume))
+    except Exception as ex:
         print('Failed to update {0} for {1}.'.format(args.options, args.volume))
 
 
